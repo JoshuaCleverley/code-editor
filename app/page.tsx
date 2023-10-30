565f5e59bfe9d4d57773d3ab4438fc7267538208
Joshua Cleverley <joshuacleverley@protonmail.com>
2023-10-30 11:27:02 +0000

    Basic Button component styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <button>New Project</button>
-      <button>Load Project</button>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <Button>New Project</Button>
+      <Button>Load Project</Button>
     </main>
   )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function Button({type = 'button', children}: Props) {
   return (
-    <button type={type} className="">
+    <button type={type} className="py-2 px-4 rounded-full bg-blue-600 hover:bg-blue-700 hover:scale-105 transition-all duration-200">
       {children}
     </button>
   )

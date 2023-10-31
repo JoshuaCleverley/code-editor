0c9fa849ff7119dc1321443a8bdd0f5c0545b472
Joshua Cleverley <joshuacleverley@protonmail.com>
2023-10-31 13:37:18 +0000

    Start laying out project area

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,17 @@
-export default function ProjectPage() {
-  return (
-    <main>
+import { Button } from "@/components/Button";
 
+export default function ProjectPage() {
+  return ( 
+    <main>
+      <div className="flex flex-row gap-4 m-4">
+        <Button>Import</Button>
+        <Button>Export</Button>
+        <Button>New File</Button>
+      </div>
+      <div>
+        {/* Tabs for different files */}
+        {/* Code editor */}
+      </div>
     </main>
   )
 }

b44c172d18aec3635cc8889c78d2bce751ef3930
Joshua Cleverley <joshuacleverley@protonmail.com>
2023-10-31 13:55:46 +0000

    (very) basic (not very good) style for code editor

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -2,15 +2,23 @@ import { Button } from "@/components/Button";
 
 export default function ProjectPage() {
   return ( 
-    <main>
-      <div className="flex flex-row gap-4 m-4">
+    <main className="bg-black min-h-screen overflow-hidden">
+      <div className="flex flex-row gap-4 p-4">
         <Button>Import</Button>
         <Button>Export</Button>
-        <Button>New File</Button>
       </div>
-      <div>
+      <div className="flex flex-col rounded-xl h-full bg-slate-900">
         {/* Tabs for different files */}
+        <div className="flex flex-row p-4 pb-0 gap-4">
+          {/* Temporary file names and buttons for testing */}
+          <Button>index.html</Button>
+          <Button>style.css</Button>
+          <Button>script.js</Button>
+        </div>
         {/* Code editor */}
+        <div>
+          <textarea className="m-4 bg-slate-800 rounded-lg text-white w-full h-[60vh]"></textarea>
+        </div>
       </div>
     </main>
   )
